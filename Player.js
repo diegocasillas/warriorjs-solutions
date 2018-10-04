4788d7ca87ee3e65f1ae311c2424bee92a82f52d
diegocasillas <diegocasillasdev@gmail.com>
2018-10-04 21:22:36 +0300

    Add solution for baby-steps-4

--- Player.js
+++ Player.js
@@ -1,21 +1,29 @@
 class Player {
   constructor() {
     this.health = 20;
+    this.currentHealth = 20;
+    this.attack = 5;
   }
 
   fullHealth(warrior) {
     return warrior.health() === this.health;
   }
 
+  takingDamage(warrior) {
+    return warrior.health() < this.currentHealth;
+  }
+
   walkOrRest(warrior) {
-    return this.fullHealth(warrior)
-          ? warrior.walk()
-          : warrior.rest();
+    return this.takingDamage(warrior) || this.fullHealth(warrior)
+      ? warrior.walk()
+      : warrior.rest();
   }
 
   playTurn(warrior) {
     warrior.feel().isEmpty()
       ? this.walkOrRest(warrior)
       : warrior.attack();
+
+    this.currentHealth = warrior.health();
   }
 }
